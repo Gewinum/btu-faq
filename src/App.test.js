7e975ac498cf133916d3f4a9b53ccb443c5f35f0
Nicholas Winterhalter <78866820+Gewinum@users.noreply.github.com>
2025-02-20 19:13:02 +0400

    test: added some tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,24 @@ import './setupTests';
 
 test('make sure header is present', () => {
   render(<App />);
-  const linkElement = screen.getByText(data.name);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(data.name);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('make sure navigation is present', () => {
+    render(<App />);
+    for (const section in data.navigation) {
+        const navigationElement = screen.getByText(data.navigation[section]);
+        expect(navigationElement).toBeInTheDocument();
+    }
+});
+
+test('make sure questions are present', () => {
+  render(<App />);
+    for (const section in data.questions) {
+        for (const question of data.questions[section]) {
+          const questionElement = screen.getByText(question[0]);
+          expect(questionElement).toBeInTheDocument();
+        }
+    }
 });
